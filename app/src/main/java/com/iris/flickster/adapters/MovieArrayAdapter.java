44813708337255b9e89a5d3f842ec10152809487
Iris Wang <iris@pinterest.com>
2016-07-21 23:59:04 -0700

    added files

---
+++ app/src/main/java/com/iris/flickster/adapters/MovieArrayAdapter.java
@@ -0,0 +1,64 @@
+package com.iris.flickster.adapters;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.iris.flickster.R;
+import com.iris.flickster.models.Movie;
+import com.squareup.picasso.Picasso;
+
+
+import java.util.List;
+
+/**
+ * Created by iris on 7/21/16.
+ */
+public class MovieArrayAdapter extends ArrayAdapter<Movie>{
+
+    private static class ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivImage;
+    }
+
+    public MovieArrayAdapter(Context context, List<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivPoster);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.ivImage.setImageResource(0);
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
+        String imagePath;
+        if (getContext().getResources().getConfiguration().orientation ==
+            Configuration.ORIENTATION_LANDSCAPE) {
+            imagePath = movie.getBackdropPath();
+        } else {
+            imagePath = movie.getPosterPath();
+        }
+        Picasso.with(getContext()).load(imagePath).into(viewHolder.ivImage);
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/iris/flickster/models/Movie.java
@@ -0,0 +1,56 @@
+package com.iris.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by iris on 7/21/16.
+ */
+public class Movie {
+
+    private String IMAGE_PATH_FORMAT = "https://image.tmdb.org/t/p/w342/%s";
+
+    String backdropPath;
+
+    String originalTitle;
+
+    String posterPath;
+
+
+    public String getBackdropPath() {
+        return String.format(IMAGE_PATH_FORMAT, backdropPath);
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getPosterPath() {
+        return String.format(IMAGE_PATH_FORMAT, posterPath);
+    }
+
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+        this.backdropPath = jsonObject.getString("backdrop_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+        this.posterPath = jsonObject.getString("poster_path");
+    }
+
+    public static ArrayList<Movie> fromJsonArray(JSONArray jsonArray) throws JSONException{
+        ArrayList<Movie> movieList = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            movieList.add(new Movie(jsonArray.getJSONObject(i)));
+        }
+        return movieList;
+    }
+}
