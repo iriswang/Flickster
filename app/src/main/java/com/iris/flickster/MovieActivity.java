f7bf281177908451871718746678737a01853e61
Iris Wang <iris@pinterest.com>
2016-07-21 23:08:52 -0700

    initial movie app with viewholder pattern implemented

--- app/src/main/java/com/iris/flickster/MovieActivity.java
+++ app/src/main/java/com/iris/flickster/MovieActivity.java
@@ -2,12 +2,61 @@ package com.iris.flickster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.iris.flickster.adapters.MovieArrayAdapter;
+import com.iris.flickster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
+
+    ArrayList<Movie> movies;
+    MovieArrayAdapter _movieArrayAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        movies = new ArrayList<>();
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+        _movieArrayAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(_movieArrayAdapter);
+
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray moviesJsonResult = null;
+                try {
+                    moviesJsonResult = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(moviesJsonResult));
+                    _movieArrayAdapter.notifyDataSetChanged();
+                    Log.d("DEBUG", movies.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable,
+                JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 }
