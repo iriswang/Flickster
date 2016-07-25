695eb2f03ed82d9b96657e8ba9faba8e44fe4a87
Iris Wang <iris@pinterest.com>
2016-07-24 20:49:45 -0700

    added swipe refresh view

--- app/src/main/java/com/iris/flickster/MovieActivity.java
+++ app/src/main/java/com/iris/flickster/MovieActivity.java
@@ -1,5 +1,7 @@
 package com.iris.flickster;
 
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,7 +26,10 @@ public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     MovieArrayAdapter _movieArrayAdapter;
     ListView lvItems;
-    String URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    final String URL =
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,43 @@ public class MovieActivity extends AppCompatActivity {
         _movieArrayAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(_movieArrayAdapter);
         fetchMovieData();
+        setUpSwipeContainer();
 
+    }
+
+    private void setUpSwipeContainer() {
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+               fetchRefreshedMovieData();
+            }
+        });
+    }
+
+    private void fetchRefreshedMovieData() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray moviesJsonResult;
+                try {
+                    moviesJsonResult = response.getJSONArray("results");
+                    movies.clear();
+                    movies.addAll(Movie.fromJsonArray(moviesJsonResult));
+                    _movieArrayAdapter.notifyDataSetChanged();
+                    swipeContainer.setRefreshing(false);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable,
+                JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 
     private void fetchMovieData() {
